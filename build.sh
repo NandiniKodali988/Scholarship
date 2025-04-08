f5207ab60a9f0966d69737dddc2c0be48c205a4c
NandiniKodali988 <nandinikodali5@outlook.com>
2025-04-07 23:24:57 -0400

    final commits

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 printf 'Would you like to push to GU domains? (y/n)? '
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
-    rsync -alvr --delete Scholarship/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/dsan-5000/
+    rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/_site
 
 else
     echo NOT PUSHING TO GU DOMAINS!
